5beaf30431fb937d9cf7df2fd654ee787d1f16dc
Jeff Kealy <jeffkealy@yahoo.com>
2016-12-15 19:38:22 -0600

    adjusted file for heroku env 2

--- server/routes/modules/decoder.js
+++ server/routes/modules/decoder.js
@@ -1,10 +1,23 @@
 var admin = require("firebase-admin");
-var firebaseServiceAccount = "/Users/jeffkealy/Documents/Prime/Fudr/server/firebase-service-account.json";
+var firebaseServiceAccount = "";
 
 if (process.env.FIREBASE != undefined) {
-  firebaseServiceAccount = process.env.FIREBASE
+  firebaseServiceAccount = admin.credential.cert
 } else {
   firebaseServiceAccount = "/Users/jeffkealy/Documents/Prime/Fudr/server/firebase-service-account.json";
+}
+
+var firebaseServices = {
+  "type": "service_account",
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url
 }
 
 admin.initializeApp({
