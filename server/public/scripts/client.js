087255f8a09511fed204d307e2e8670c0406321c
Jeff Kealy <jeffkealy@yahoo.com>
2016-12-23 12:22:36 -0600

    added swipe feature using ngTouch

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngRoute', 'ngMaterial', 'firebase']);
+var app = angular.module('myApp', ['ngRoute', 'ngMaterial', 'firebase', 'ngTouch']);
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
